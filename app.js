0e7e1e9a7e14dffe4a912617899afe6c8e5b87ea
Joel Foster <joelsfoster@gmail.com>
2016-11-19 00:00:35 -0500

    reverted GET route change

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 // Route the app to index.html
-app.get('', function(req, res) {
+app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
